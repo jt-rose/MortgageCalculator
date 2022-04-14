a9ea8b65b73f864b61d05f1676a23dd4fe5b9e66
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-14 17:58:43 -0400

    initial commit

---
+++ src/com/mortgagecalculator/Main.java
@@ -0,0 +1,15 @@
+package com.mortgagecalculator;
+
+public class Main {
+
+    // code along for Mosh Hamedani's Learn Java course
+    // https://www.youtube.com/watch?v=eIrMbAQSU34
+    public static void main(String[] args) {
+	byte age = 30;
+    long viewsCount = 3_999_999_999L;
+    float price = 10.99F;
+    char letter = 'A';
+    boolean isEligible = false;
+    System.out.println("Hello Mortgage Calculator");
+    }
+}
